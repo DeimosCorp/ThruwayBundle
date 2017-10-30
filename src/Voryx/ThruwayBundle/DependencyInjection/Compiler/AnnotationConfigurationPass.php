5927b1fbf1b0fc6dff290c769cacd860c14e2a88
LethaK Maas <lethak@users.noreply.github.com>
2017-10-30 03:33:44 +0100

    Kernel is not registered in Worker
    https://github.com/voryx/ThruwayBundle/issues/45

--- src/Voryx/ThruwayBundle/DependencyInjection/Compiler/AnnotationConfigurationPass.php
+++ src/Voryx/ThruwayBundle/DependencyInjection/Compiler/AnnotationConfigurationPass.php
@@ -53,7 +53,8 @@ class AnnotationConfigurationPass implements CompilerPassInterface
 
             if ($class->implementsInterface('Symfony\Component\DependencyInjection\ContainerAwareInterface')) {
                 $container->setDefinition($serviceId, $definition)
-                    ->addMethodCall('setContainer', [new Reference('thruway_container')]);
+                    //->addMethodCall('setContainer', [new Reference('thruway_container')]);
+                    ->addMethodCall('setContainer', [new Reference('service_container')]);
             } else {
                 $container->setDefinition($serviceId, $definition);
             }
